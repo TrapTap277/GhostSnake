91d3791d141ab611899ad54212e840324cad0fd1
TrapTap277 <sway8318@gmail.com>
2024-09-06 20:55:35 +0300

    Added Input Installer

---
+++ Assets/_Scripts/Installers/InputInstaller.cs
@@ -0,0 +1,34 @@
+﻿using _Scripts.Snake.MoveLogic;
+using UnityEngine;
+using Zenject;
+
+namespace _Scripts.Installers
+{
+    public class InputInstaller : MonoInstaller
+    {
+        [SerializeField] private PlayerConfig playerConfig;
+
+        public override void InstallBindings()
+        {
+            playerConfig.InitMoveBehaviours();
+
+            Container.Bind<IMove>()
+                .WithId(PlayerConfig.SHIFTING_ID)
+                .To<Shifting>()
+                .FromInstance(playerConfig.Shifting)
+                .AsSingle();
+            Container.Bind<IMove>()
+                .WithId(PlayerConfig.MOVE_ID)
+                .To<Walk>()
+                .FromInstance(playerConfig.Walk)
+                .AsSingle();
+            Container.Bind<IMove>()
+                .WithId(PlayerConfig.RUN_ID)
+                .To<Run>()
+                .FromInstance(playerConfig.Run)
+                .AsSingle();
+
+            Container.BindInterfacesTo<InputReader>().AsSingle();
+        }
+    }
+}
